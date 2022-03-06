8217b6756222ed1e302cfcc7df1bd796473b2661
xubin34 <xubin34@meituan.com>
2022-03-06 15:17:37 +0800

    init

---
+++ MyPython/day01/打印好看的信息卡.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+name = input("name:")
+age = input("age:")
+hometown = input("hometown:")
+hobbie = input("hobbie:")
+
+
+
+msg = f'''
+------------ info of {name} -----------
+Name  	: {name}
+Age   	: {age}
+job 	: {hometown}
+Hobbie	: {hobbie}
+------------- end -----------------
+'''
+print(msg)
